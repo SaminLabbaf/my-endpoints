886cdca55b621aaf11cf9a3864c39c8db1be0c11
samin1994 <saminlabbaf1994@gmail.com>
2025-04-09 00:51:22 -0400

    first commit

---
+++ backend/main.js
@@ -0,0 +1,43 @@
+// Set up the API
+const express = require('express')
+var cors = require('cors');
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3007
+
+// Make it available for public access
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    next();
+});
+
+app.use(cors());
+app.options("*", cors());
+
+app.set('json spaces', 2)
+app.use(bodyParser.json({
+    limit: "50mb"
+}))
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+// Listen to outside connection
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}. Control+C to exit.`)
+})
+
+// Spit out data
+
+app.get('/', (request, response) => {
+    response.json(
+        {
+            info: 'Backend for GBC Library, set up by Chris K!'
+        }
+    )
+})
