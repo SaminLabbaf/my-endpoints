532d7efc95d36caca5a34db270c6f4791eea2907
SaminLabbaf <saminlabbaf1994@gmail.com>
2025-04-15 16:43:12 -0400

    added ratings endpoints

--- backend/main.js
+++ backend/main.js
@@ -114,3 +114,43 @@ app.post("/v1/recipes", (request, response) => {
         }
     });
 });
+
+// RATINGS ENDPOINTS
+app.get("/v1/ratings", (request, response) => {
+    pool.query(`SELECT CONCAT(Users.fname, ' ', Users.lname) AS user, Recipes.title AS recipe , rating, comment from Ratings 
+JOIN Users on Users.user_id = Ratings.user
+JOIN Recipes on Recipes.recipe_id = Ratings.recipe ORDER BY rating_id`, [], (error, result) => {
+        if (error) {
+            response.status(500).json({ error: "Error fetching data" });
+        }
+        else {
+            response.json(
+                {
+                    status: "success",
+                    data: result
+                }
+            )
+        }
+    });
+}
+);
+
+app.post("/v1/ratings", (request, response) => {
+    const { user_id, recipe_id, rating, comment } = request.body;
+    pool.query(`INSERT INTO Ratings (user, recipe, rating, comment, created_at) VALUES
+        (?, ?, ?, ?, NOW())`, [user_id, recipe_id, rating, comment], (error, result) => {
+        if (error) {
+            response.status(500).json({ error: error.message });
+        }
+        else {
+            response.json(
+                {
+                    status: "success",
+                    message: "New rating added successfully"
+                }
+            )
+        }
+    }
+    );
+});
+
